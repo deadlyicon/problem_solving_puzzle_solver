6ceb5532a04561143b57764c25fb71167dbc261e
Jared Grippe <jared@deadlyicon.com>
2015-07-18 14:14:00 -0700

    WIP

--- Gemfile
+++ Gemfile
@@ -2,5 +2,6 @@ source "https://rubygems.org"
 
 ruby '2.2.2'
 
+gem 'selenium-webdriver'
 gem 'capybara'
 gem 'pry-byebug'
--- solver.rb
+++ solver.rb
@@ -1,6 +1,7 @@
 Dir.chdir File.expand_path('..', __FILE__)
+require 'timeout'
 require 'bundler'
-Bundle.require
+Bundler.require
 
 Capybara.register_driver :selenium do |app|
   Capybara::Selenium::Driver.new(app, :browser => :chrome)
@@ -11,19 +12,43 @@ Capybara.current_driver = :selenium
 
 class Solver
   include Capybara::DSL
+  URL = 'http://www.nytimes.com/interactive/2015/07/03/upshot/a-quick-puzzle-to-test-your-problem-solving.html'
 
   def initialize
     @number = 0
-    visit 'http://www.nytimes.com/interactive/2015/07/03/upshot/a-quick-puzzle-to-test-your-problem-solving.html'
-    3.times{ try_next_sequence }
+    visit URL
+    puts "sequence, result"
+    loop{ try_next_sequence }
   end
 
-  def sequence
+  def sequence_as_string
     sprintf("%03d", @number)
   end
 
+  def sequence
+    sequence_as_string.split(//)
+  end
+
+  def answers
+    @answers ||= []
+  end
+
   def try_next_sequence
+    page.execute_script "window.scrollBy(0,9999999999999)"
     inputs = all('#g-input .g-num')
-    binding.pry
+    sequence.each_with_index do |n, index|
+      inputs[index].set(n)
+    end
+    click_button 'Check'
+    page.execute_script "$('html,body').stop()"
+    if !page.has_selector?('.g-yours .g-answer', count: @number+1)
+      raise 'failed to find new answer :('
+    end
+    @answers = all('.g-yours .g-answer').map(&:text)
+    puts "#{sequence_as_string}, #{answers.last}"
+    @number += 1
   end
 end
+
+
+Solver.new
